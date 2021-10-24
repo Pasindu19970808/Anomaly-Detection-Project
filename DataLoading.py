6d830efba91fbe6822d7c1faa7794b4777a2f0a4
Aktuarius <paul.ruckert@gmail.com>
2021-10-24 19:56:06 +0800

    Load data as Data Frames - Splits into training/testing data based on file name

---
+++ DataLoading.py
@@ -0,0 +1,36 @@
+import os
+import matplotlib as plt
+import seaborn
+import numpy as np
+import pandas as pd
+import re
+
+# define path to data directory
+file_path = os.path.join(os.getcwd(), 'KDD-Cup', 'data')
+print(file_path)
+
+file_to_load = os.path.join(file_path, os.listdir(file_path)[1])
+print(file_to_load)
+
+train, test = split_data(file_path, 1)
+train.head()
+test.head()
+
+
+def split_data(file_path, index):
+    file_name = os.listdir(file_path)[index]
+    test_data_start_pt = int(re.findall(
+        r'[0-9]*.txt', file_name)[0].split('.')[0])
+    data = pd.read_csv(os.path.join(file_path, os.listdir(file_path)[index]))
+    train_data = data[0:test_data_start_pt]
+    test_data = data[test_data_start_pt+1:len(data)]
+
+    return train_data, test_data
+
+# # Above, initially set to only load first file into "data" and return training/testing data frames
+#  in future could beextended to something like this
+# for index in range(0,# files):
+# #     train, test = split_data(file_path, index)
+# #     train model
+# #     test model
+# #     append identified anomoly to list of anomoly items
